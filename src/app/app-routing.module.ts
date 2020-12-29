20a8d54deb67eb312d3f6a8d3a773fbddf2062be
Gourav Sachdeva <gouravsachdeva95@gmail.com>
2020-12-29 17:42:28 +0530

    Deleting extra component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SliderComponent } from './slider/slider.component';
 
 const routes: Routes = [
-  { path: 'slider', component: SliderComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SliderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
